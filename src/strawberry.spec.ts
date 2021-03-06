ef9fdfb909b624741a4557a45480f7e0028edb5f
大友凜 <ootomorin@MacBook-Air.local>
2021-03-06 13:27:54 +0900

    ペアプロ二回目終了時点

--- src/strawberry.spec.ts
+++ src/strawberry.spec.ts
@@ -1,6 +1,40 @@
-import { testTarget } from './strawberry';
+import { Strawberry, format } from './strawberry';
 it('テストが動くかどうか', () => {});
 
-it('あまおうを入力して、あまおうを返すこと', () => {
-    expect(testTarget('あまおう')).toBe('あまおう');
+describe('Strawberry', () => {
+    const variety = 'いちごの品種名';
+    const size = 'いちごのサイズ';
+    let strawberry: Strawberry;
+
+    beforeEach(() => {
+        strawberry = new Strawberry(variety, size);
+    });
+
+    it('品種とサイズを入力して、Strawberrオブジェクトを取得できること。', () => {
+        expect(strawberry).toBeTruthy();
+    });
+
+    it('入力した品種の文字列とStrawberrオブジェクトの品種の文字列が一致すること', () => {
+        expect(strawberry.variety).toBe(variety);
+    });
+
+    it('入力したサイズの文字列とStrawberrオブジェクトのサイズの文字列が一致すること', () => {
+        expect(strawberry.size).toBe(size);
+    });
+});
+
+describe('format', () => {
+    it('品種:あまおう、サイズ:Sを入力し、文字列表現「あまおう: S」を取得できること', () => {
+        const variety = 'あまおう';
+        const size = 'S';
+        const strawberry = new Strawberry(variety, size);
+        expect(format(strawberry)).toBe('あまおう: S');
+    });
+
+    it('品種:とちおとめ、サイズ:Mを入力し、文字列表現「あまおう: S」を取得できること', () => {
+        const variety = 'とちおとめ';
+        const size = 'M';
+        const strawberry = new Strawberry(variety, size);
+        expect(format(strawberry)).toBe('とちおとめ: M');
+    });
 });
--- src/strawberry.ts
+++ src/strawberry.ts
@@ -1,3 +1,29 @@
-export function testTarget(variety: string) {
-    return variety;
+// const VARIETY = {
+//     あまおう: 'あまおう',
+//     とちおとめ: 'とちおとめ',
+//     もういっこ: 'もういっこ',
+// } as const;
+// type VARIETY_TYPE = keyof typeof VARIETY;
+
+// const SIZE = {
+//     S: 'S',
+//     M: 'M',
+//     L: 'L',
+//     LL: 'LL',
+// } as const;
+// type SIZE_TYPE = keyof typeof SIZE;
+
+export class Strawberry {
+    variety: string;
+    size: string;
+
+    constructor(variety: string, size: string) {
+        this.variety = variety;
+        this.size = size;
+    }
+}
+
+// FIXME 関数名をより具体的な名前に修正すること。
+export function format(strawberry: Strawberry) {
+    return `${strawberry.variety}: ${strawberry.size}`;
 }
